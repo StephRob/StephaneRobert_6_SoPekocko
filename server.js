b715a168692909c4729982bd8a6c06ff960b4516
Stéphane Robert <rob.steph@free.fr>
2021-06-18 10:39:27 +0200

    Add server file

---
+++ server.js
@@ -0,0 +1,54 @@
+// Import des packages
+const http = require('http');
+//Import de l'application express
+const app = require('./app');
+
+// Renvoie d'un port valide, (number ou string)
+const normalizePort = val => {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+
+// Configuration du port à écouter (port par défaut de la plateforme de déploiement ou port 3000)
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+// Recherche et gestion des erreurs de manière appropriée
+const errorHandler = error => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const server = http.createServer(app);
+
+// Enregistrement d'un écouteur d'évènement, consignant le port ou le canal nommé sur lequel le serveur s'exécute dans la console
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
+
+server.listen(port);
